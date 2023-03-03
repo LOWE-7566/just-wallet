0e63c19ffd84c78ee0a90ffd5a4aa24e270ffb18
lowecainandaya <loweandaya7566@gmail.com>
2023-03-03 18:01:18 +0800

    norrowed-the-project

--- es6/checkAddress.js
+++
@@ -1,9 +0,0 @@
-function isValidAddress(address) {
-    const addressRegExp = /^0x[a-fA-F0-9]{40}$/g;
-    const match = address.trim().match(addressRegExp);
-    const valid = match && match.length > 0 ? true : false;
-    const value = address;
-    const validAddress = match ? match[0] : undefined;
-    return { address: validAddress, value, valid };
-}
-export default isValidAddress;
